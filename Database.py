46814d1ced05bf5f11275db6bd4ab883477267e9
imrannmansari <imrannmansari@gmail.com>
2018-04-27 15:24:57 +0530

    Added Add2Cart Feature in UI with data from Database.py

--- Database.py
+++ Database.py
@@ -4,53 +4,185 @@ class Constants:
 	KEY_RESTAURANT_NAME = "NAME"
 	KEY_RESTAURANT_LOCATION = "LOCATION"
 	KEY_RESTAURANT_DELIVERY_CHARGES = "CHARGES"
-
+	KEY_RESTAURANT_LOGO = "LOGO"
+	KEY_MENU_ITEM_ID = "ITEM_ID"
+	KEY_MENU_ITEM_COST = "COST"
+	KEY_MENU_CUISINE_TYPE = "CUISINE_TYPE"
 
 restaurants = [
 			{
 				Constants.KEY_RESTAURANT_ID : 0,
-				Constants.KEY_RESTAURANT_NAME : "Paradise",
+				Constants.KEY_RESTAURANT_NAME : "PizzaHut",
 				Constants.KEY_RESTAURANT_LOCATION : "Patny",
-				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40"
+				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40",
+				Constants.KEY_RESTAURANT_LOGO : "/static/images/restaurent-logo1.jpg"
 			},
 			{
 				Constants.KEY_RESTAURANT_ID : 1,
-				Constants.KEY_RESTAURANT_NAME : "Alpha",
+				Constants.KEY_RESTAURANT_NAME : "Subway",
 				Constants.KEY_RESTAURANT_LOCATION : "Patny",
-				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40"
+				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40",
+				Constants.KEY_RESTAURANT_LOGO : "/static/images/restaurent-logo2.jpg"
 			},
 			{
 				Constants.KEY_RESTAURANT_ID : 2,
-				Constants.KEY_RESTAURANT_NAME : "Elephant",
+				Constants.KEY_RESTAURANT_NAME : "Barista",
 				Constants.KEY_RESTAURANT_LOCATION : "Patny",
-				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40"
+				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40",
+				Constants.KEY_RESTAURANT_LOGO : "/static/images/restaurent-logo3.jpg"
 			},
 			{
 				Constants.KEY_RESTAURANT_ID : 3,
-				Constants.KEY_RESTAURANT_NAME : "Biryani Mahal",
+				Constants.KEY_RESTAURANT_NAME : "Papa John's",
 				Constants.KEY_RESTAURANT_LOCATION : "Patny",
-				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40"
+				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40",
+				Constants.KEY_RESTAURANT_LOGO : "/static/images/restaurent-logo4.jpg"
 			},
 			{
 				Constants.KEY_RESTAURANT_ID : 4,
-				Constants.KEY_RESTAURANT_NAME : "Pizza Hut",
+				Constants.KEY_RESTAURANT_NAME : "Dominos",
 				Constants.KEY_RESTAURANT_LOCATION : "Patny",
-				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40"
+				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40",
+				Constants.KEY_RESTAURANT_LOGO : "/static/images/restaurent-logo5.jpg"
+			},
+			{
+				Constants.KEY_RESTAURANT_ID : 5,
+				Constants.KEY_RESTAURANT_NAME : "KFC",
+				Constants.KEY_RESTAURANT_LOCATION : "Patny",
+				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40",
+				Constants.KEY_RESTAURANT_LOGO : "/static/images/restaurent-logo6.jpg"
+			},{
+				Constants.KEY_RESTAURANT_ID : 6,
+				Constants.KEY_RESTAURANT_NAME : "Paradise",
+				Constants.KEY_RESTAURANT_LOCATION : "Patny",
+				Constants.KEY_RESTAURANT_DELIVERY_CHARGES : "40",
+				Constants.KEY_RESTAURANT_LOGO : "/static/images/restaurent-logo7.jpg"
 			}
-
 		]
-'''
+
 restaurantMenu = [
 	{
-		"restaurantID" : 0,
+		Constants.KEY_RESTAURANT_ID : 0,
 		"menu" : [
 			{
-				""
+				Constants.KEY_MENU_ITEM_ID : "Margretta Pizza",
+				Constants.KEY_MENU_ITEM_COST : "220",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Double Paneer Twist Pizza",
+				Constants.KEY_MENU_ITEM_COST : "240",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Spicy Chicken Pizza",
+				Constants.KEY_MENU_ITEM_COST : "330",
+				Constants.KEY_MENU_CUISINE_TYPE :"NON-VEG" 
+			}
+		]
+	},{
+		Constants.KEY_RESTAURANT_ID : 1,
+		"menu" : [
+			{
+				Constants.KEY_MENU_ITEM_ID : "Biryani",
+				Constants.KEY_MENU_ITEM_COST : "220",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Pizza",
+				Constants.KEY_MENU_ITEM_COST : "240",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Noodles",
+				Constants.KEY_MENU_ITEM_COST : "130",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			}
+		]
+	},{
+		Constants.KEY_RESTAURANT_ID : 2,
+		"menu" : [
+			{
+				Constants.KEY_MENU_ITEM_ID : "Biryani",
+				Constants.KEY_MENU_ITEM_COST : "220",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Pizza",
+				Constants.KEY_MENU_ITEM_COST : "240",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Noodles",
+				Constants.KEY_MENU_ITEM_COST : "130",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			}
+		]
+	},{
+		Constants.KEY_RESTAURANT_ID : 3,
+		"menu" : [
+			{
+				Constants.KEY_MENU_ITEM_ID : "Margretta Pizza",
+				Constants.KEY_MENU_ITEM_COST : "220",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Double Paneer Twist Pizza",
+				Constants.KEY_MENU_ITEM_COST : "240",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Spicy Chicken Pizza",
+				Constants.KEY_MENU_ITEM_COST : "330",
+				Constants.KEY_MENU_CUISINE_TYPE :"NON-VEG" 
+			}
+		]
+	},{
+		Constants.KEY_RESTAURANT_ID : 4,
+		"menu" : [
+			{
+				Constants.KEY_MENU_ITEM_ID : "Margretta Pizza",
+				Constants.KEY_MENU_ITEM_COST : "220",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Double Paneer Twist Pizza",
+				Constants.KEY_MENU_ITEM_COST : "240",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Spicy Chicken Pizza",
+				Constants.KEY_MENU_ITEM_COST : "330",
+				Constants.KEY_MENU_CUISINE_TYPE :"NON-VEG" 
+			}
+		]
+	},{
+		Constants.KEY_RESTAURANT_ID : 5,
+		"menu" : [
+			{
+				Constants.KEY_MENU_ITEM_ID : "Biryani",
+				Constants.KEY_MENU_ITEM_COST : "220",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Pizza",
+				Constants.KEY_MENU_ITEM_COST : "240",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Noodles",
+				Constants.KEY_MENU_ITEM_COST : "130",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			}
+		]
+	},{
+		Constants.KEY_RESTAURANT_ID : 6,
+		"menu" : [
+			{
+				Constants.KEY_MENU_ITEM_ID : "Biryani",
+				Constants.KEY_MENU_ITEM_COST : "220",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Pizza",
+				Constants.KEY_MENU_ITEM_COST : "240",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
+			},{
+				Constants.KEY_MENU_ITEM_ID : "Noodles",
+				Constants.KEY_MENU_ITEM_COST : "130",
+				Constants.KEY_MENU_CUISINE_TYPE :"VEG" 
 			}
 		]
 	}
 ]
-'''
+
 	
 def addRestaurant(name, location, delivery):
 	resid = int(restaurants[-1][Constants.KEY_RESTAURANT_ID]) + 1
--- Hello.py
+++ Hello.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+from Database import *
 app = Flask(__name__, static_path='/static')
 
 @app.route('/')
@@ -8,7 +9,7 @@ def hello_world():
 
 @app.route('/index')
 def show_index():
-	return render_template("index.html")
+	return render_template("index.html",restaurants=restaurants)
 
 @app.route('/index/<name>')
 def show_name(name):
@@ -21,6 +22,10 @@ def show_name(name):
 def show_restaurants():
 	return render_template("restaurants.html")
 
+@app.route('/restaurants/<restaurant_id>')
+def show_restaurant_menu(restaurant_id):
+	print("called restaurant menu")
+	return render_template("menu.html",menu=getRestaurantMenu(restaurant_id),restaurant=getRestaurant(restaurant_id))
 
 @app.route('/order')
 def show_order():
@@ -41,9 +46,25 @@ def show_login():
 def show_admin():
 	return render_template("register.html")
 
+@app.route('/checkout')
+def show_checkout():
+	return render_template("checkout.html")
+
+@app.route('/order-list')
+def show_orderList():
+	return render_template("order-list.html")
+
 ######### this is a small change made by tdsk
 
+def getRestaurantMenu(restaurant_id):
+	for restaurant in restaurantMenu:
+		if restaurant['ID'] is int(restaurant_id):
+			return restaurant['menu']
 
+def getRestaurant(restaurant_id):
+	for restaurant in restaurants:
+		if restaurant['ID'] is int(restaurant_id):
+			return restaurant
 
 if __name__ == '__main__':
    app.run(debug=True)
